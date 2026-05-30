2a188d2457d830c66b6764e0253dda8013f37f2c
Asna Farooqui <a.farooqui.26918@khi.iba.edu.pk>
2026-05-31 02:59:55 +0500

    Mail log

--- backend/mail.js
+++ backend/mail.js
@@ -11,4 +11,11 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Verify Error:", error);
+  } else {
+    console.log("SMTP Server Ready");
+  }
+});
 module.exports = transporter;
