249b209d10c3a571b277a1aeb383a85e0382b7cd
Asna Farooqui <a.farooqui.26918@khi.iba.edu.pk>
2026-05-31 03:23:34 +0500

    Mail change

--- backend/mail.js
+++ backend/mail.js
@@ -3,12 +3,22 @@ const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 
+// const transporter = nodemailer.createTransport({
+//   service: "Gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
 });
 
 transporter.verify((error, success) => {
